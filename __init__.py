339b3b4ab5ae3f9f4503d56ccd89f3434196201f
ericseidel <34065018+ericseidel@users.noreply.github.com>
2020-12-02 10:14:14 -0500

    PLAT3-473 added new  (#1)
    * PLAT3-473 added new exception type for when max apps have been reached
    
    * PLAT3-473 changed code to 402 payment required
    
    * PLAT3-473 added missing import

--- __init__.py
+++ __init__.py
@@ -3,4 +3,5 @@ This module defines all common exception types for Lumavate projects.
 """
 from .exceptions import \
     LumavateException, ApiException, AuthorizationException, NotFoundException, \
-    ValidationException, InvalidOperationException, AsyncException
+    ValidationException, InvalidOperationException, AsyncException, \
+    MaxAppException
